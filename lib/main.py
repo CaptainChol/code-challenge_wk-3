7ba7c9f646d36bf03451b076342ce891e6c32ab3
CaptainChol <emmanuel.marial@student.moringaschool.com>
2023-09-13 22:58:13 +0300

    completed work

--- lib/main.py
+++ lib/main.py
@@ -1,7 +1,7 @@
 from sqlalchemy import String, Integer, MetaData, Table, Column, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 
 metadata = MetaData()
 Base = declarative_base(metadata=metadata)
@@ -14,7 +14,6 @@ restuarant_customer = Table(
     Column("restaurant_id", Integer, ForeignKey("restaurants.id"), primary_key=True),
     extend_existing=True,
 )
-
 
 class Restaurant(Base):
     __tablename__ = "restaurants"
@@ -32,12 +31,20 @@ class Restaurant(Base):
         return f"RESTAURANT: {self.name} {self.price}"
 
     def reviews(self, session):
-      return session.query(Review).filter(Review.restuarant.id==self.id).all()
-    
-    def customers(self, session):
-      customer_ids=session.query(review.customer_id).filter(review.resturant_id==self).all()
-      return [session.query(Customers).get(customer_id[0]) for customer_id in customer_ids]
+        return session.query(Review).filter(Review.restaurant_id == self.id).all()
 
+    def all_reviews(self, session):
+        review_strings = []
+        for review in self.reviews(session):
+            customer_full_name = review.customer(session).full_name()
+            star_rating = review.star_rating
+            review_str = f"Review for {self.name} by {customer_full_name}: {star_rating} stars."
+            review_strings.append(review_str)
+        return review_strings
+
+    @classmethod
+    def fanciest(cls, session):
+        return session.query(cls).order_by(cls.price.desc()).first()
 
 class Review(Base):
     __tablename__ = "reviews"
@@ -47,12 +54,12 @@ class Review(Base):
     customer_id = Column(Integer, ForeignKey("customers.id"), nullable=False)
     restaurant_id = Column(Integer, ForeignKey("restaurants.id"), nullable=False)
 
-  def customer(self, session):
-   return session.query(Customers).filter_by(id=self.customer_id).first()
+    def customer(self, session):
+        return session.query(Customers).filter_by(id=self.customer_id).first()
 
-  def restaurant(self, session):
-    return session.query(Restaurant).filter_by(id=self.restaurant_id).first()
 
+    def restaurant(self, session):
+        return session.query(Restaurant).filter_by(id=self.restaurant_id).first()
 
 class Customers(Base):
     __tablename__ = "customers"
@@ -66,7 +73,40 @@ class Customers(Base):
         self.first_name = first_name
         self.last_name = last_name
 
+    def get_reviews(self, session):
+        return session.query(Review).filter(Review.customer_id == self.id).all()
+
+    def get_restaurants(self, session):
+        restaurant_ids = session.query(Review.restaurant_id).filter(Review.customer_id == self.id).all()
+        return [session.query(Restaurant).get(restaurant_id[0]) for restaurant_id in restaurant_ids]
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def favorite_restaurant(self, session):
+        max_rating = session.query(func.max(Review.star_rating)).filter_by(customer_id=self.id).scalar()
+        if max_rating is not None:
+          favorite_review = (
+            session.query(Review)
+            .filter_by(customer_id=self.id, star_rating=max_rating)
+            .first()
+        )
+        if favorite_review:
+            return favorite_review.restaurant(session)
+        return None
+
+
+    def add_reviews(self, star_rating, restaurant_id, customer_id, session):
+        rev = Review(star_rating=star_rating, restaurant_id=restaurant_id, customer_id=customer_id)
+        session.add(rev)
+        session.commit()
+        return rev
+
+    def delete_reviews(self, restaurant, session):
+         session.query(Review).filter(
+            (Review.customer_id == self.id) & ( Review.restaurant_id == restaurant.id)
+        ).delete()
+         session.commit() 
+
     def __repr__(self):
         return f"CUSTOMER: {self.first_name} {self.last_name}"
-
-   
--- lib/seed.py
+++ lib/seed.py
@@ -14,32 +14,119 @@ if __name__ == "__main__":
     # session.query(Restaurant).delete()
     # session.query(Customers).delete() 
     # session.query(Review).delete()
-    fake = Faker()
+    # fake = Faker()
 
-    customers=[]
-    for i in range(50):
-      customer=Customers(first_name=fake.first_name(), last_name=fake.last_name())
-      session.add(customer)
-      session.commit()
-      customers.append(customer)
+    # customers=[]
+    # for i in range(50):
+    #   customer=Customers(first_name=fake.first_name(), last_name=fake.last_name())
+    #   session.add(customer)
+    #   session.commit()
+    #   customers.append(customer)
 
-    restaurants =[]
-    for i in range(25):
-      restaurant = Restaurant(name=fake.company(), price=random.randint(10, 50))
-      session.add(restaurant)
-      session.commit()
-      restaurants.append(restaurant)
+    # restaurants =[]
+    # for i in range(25):
+    #   restaurant = Restaurant(name=fake.company(), price=random.randint(10, 50))
+    #   session.add(restaurant)
+    #   session.commit()
+    #   restaurants.append(restaurant)
 
-    reviews = []
-    for i in range(100):
-      for customer in customers:
-        review = Review(
-        star_rating= random.randint(0,5),
-        customer_id=customer.id,
-        restaurant_id=restaurants[random.randint(0, len(restaurants)-1)].id
-      )
+    # reviews = []
+    # for i in range(100):
+    #   for customer in customers:
+    #     review = Review(
+    #     star_rating= random.randint(0,5),
+    #     customer_id=customer.id,
+    #     restaurant_id=restaurants[random.randint(0, len(restaurants)-1)].id
+    #   )
       
-      session.add(review)
-      session.commit()
+    #   session.add(review)
+    #   session.commit()
       
-      reviews.append(review)
+    #   reviews.append(review)
+      
+      
+      # Retrieving All Reviews for a Restaurant   
+    restaurant = session.query(Restaurant).filter_by(name="Davis PLC").first()
+    if restaurant:
+          print(f"Reviews for {restaurant.name}:")
+          reviews = restaurant.all_reviews(session)
+          for review in reviews:
+              print(review)
+    else:
+          print("Restaurant not found.")
+          
+          
+      # Finding the Fanciest Restaurant      
+    
+    fanciest = Restaurant.fanciest(session)
+    if fanciest:
+        print(f"The fanciest restaurant is {fanciest.name} with a price of {fanciest.price}")
+    else:
+        print("No restaurants found.")
+          
+      # Retrieving Customer Information for a Review   
+    review = session.query(Review).first() 
+    customer = review.customer(session)
+    if customer:
+          print(f"Customer for this review: {customer.full_name()}")
+    else:
+          print("Customer not found.") 
+          
+          
+      # Retrieving Restaurant Information for a Review      
+    restaurant = review.restaurant(session)
+    if restaurant:
+          print(f"Restaurant for this review: {restaurant.name}")
+    else:
+          print("Restaurant not found.")   
+          
+          
+      #  Retrieving All Reviews Written by a Customer
+    customer_reviews = customer.get_reviews(session)
+    if customer_reviews:
+          print(f"Reviews by {customer.full_name()}:")
+          for review in customer_reviews:
+              print(f"{review.restaurant(session).name}: {review.star_rating} stars")
+              print("*" * 30)
+    else:
+          print(f"{customer.full_name()} hasn't written any reviews.")
+          
+          
+      # Retrieving All Restaurants Reviewed by a Customer
+    customer_restaurants = customer.get_restaurants(session)
+    if customer_restaurants:
+          print(f"Restaurants reviewed by {customer.full_name()}:")
+          for restaurant in customer_restaurants:
+              print(restaurant.name)
+    else:
+          print(f"{customer.full_name()} hasn't reviewed any restaurants.")
+          
+      #  Displaying the Full Name of a Customer    
+    # full_name = customer.full_name()
+    # print(f"Customer's Full Name: {full_name}") 
+      
+      
+      # Finding a Customer's Favorite Restaurant      
+    favorite = customer.favorite_restaurant(session)
+    if favorite:
+          print(f"{customer.full_name()}'s favorite restaurant is {favorite.name}")
+    else:
+          print(f"{customer.full_name()} hasn't reviewed any restaurants.") 
+          
+     
+      # Adding a New Review   
+    star_rating = 4  
+    restaurant_id = restaurant.id
+    customer_id = customer.id
+    new_review = customer.add_reviews(star_rating, restaurant_id, customer_id, session)
+    print(f"Review added with ID: {new_review.id}")  
+      
+      
+    # Deleting a Review by a Customer  
+    customer.delete_reviews(restaurant, session)
+    print(f"Review by {customer.full_name()} for {restaurant.name} deleted.") 
+         
+
+          
+
+
